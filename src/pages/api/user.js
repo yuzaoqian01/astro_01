2a8ea6acfce6031f8a1fc30539a6079454835c42
zql <zql@zqldeMac-mini.local>
2026-01-14 18:17:01 +0800

    feat header style

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,15 +1,8 @@
 export async function GET({ params, request }) {
-//   return new Response(
-//     JSON.stringify({
-//       name: "Astro",
-//       url: "https://astro.build/",
-//     }),
-//   );
-
-
-//   const response = await fetch(
-//     "https://docs.astro.build/assets/full-logo-light.png",
-//   );
-
-//   return new Response(await response.arrayBuffer());
+  return new Response(
+    JSON.stringify({
+      name: "Astro",
+      url: "https://astro.build/",
+    }),
+  );
 }
